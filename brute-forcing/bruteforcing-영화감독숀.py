acbe8f93224b0172239b2b65be2953a68bef6200
dowoon <dowoon@dowoonui-MacBookPro.local>
2020-12-31 17:08:52 +0900

    update

---
+++ brute-forcing/bruteforcing-영화감독숀.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = []
+k = 0
+i = 666
+
+while True:
+    if len(arr) == n:
+        break
+
+    if "666" in str(i):
+        arr.append(i)
+        k+=1
+
+    i+=1
+print(arr[n-1])
