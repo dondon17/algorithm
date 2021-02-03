9c8aae38a9ad2557eb3b4ebd08cc316146930494
dondon <qwerqw889@ajou.ac.kr>
2021-02-03 13:37:39 +0900

    나동빈 큰수의 법칙 풀이2

--- greedy/nadongbin-bignum.py
+++ greedy/nadongbin-bignum.py
@@ -1,28 +1,49 @@
-def solution(n, m, k):
+# def solution(n, m, k):
+#     answer = 0
+#     cnt, tmp = 0, 0
+#     nums = list(map(int, input().split(" ")))
+#     nums.sort(reverse=True)
+
+#     _max = nums[0]
+#     _next = nums[1]
+
+#     while True:
+#         if cnt >= m:
+#             break
+
+
+#         if tmp < k:
+#             answer += _max
+#             tmp += 1
+#         elif tmp == k:
+#             tmp = 0
+#             answer += _next
+
+#         cnt += 1 
+#         print(cnt, answer)
+#     return answer
+
+# n, m, k = map(int, input().split(" "))
+# print(solution(n,m,k))
+
+
+
+#다른 풀이
+def solution(n,m,k):
     answer = 0
-    cnt, tmp = 0, 0
-
     nums = list(map(int, input().split(" ")))
-    nums.sort(reverse=True)
+    nums.sort()
 
-    _max = nums[0]
-    _next = nums[1]
+    _max = nums[n-1]
+    _next = nums[n-2]
 
-    while True:
-        if cnt >= m:
-            break
+    # 가장 큰 수가 더해지는 횟수
+    cnt = m - m//(k+1)
 
-        if tmp < k:
-            answer += _max
-            tmp += 1
-        elif tmp == k:
-            tmp = 0
-            answer += _next
-
-        cnt += 1 
-        print(cnt, answer)
-    return answer
+    # 두번째 수가 더해지는 횟수
+    cnt2 = m//(k+1)
+    answer = cnt*_max + cnt2*_next 
+    return answer    
 
 n, m, k = map(int, input().split(" "))
 print(solution(n,m,k))
-
