22ff102cb6eed6def38887e759298a9913a240cd
dondon <qwerqw889@ajou.ac.kr>
2021-03-22 23:35:19 +0900

    update

--- samsung sw/level2/alphazip.py
+++ samsung sw/level2/alphazip.py
@@ -1,13 +1,21 @@
-def solution(word, c, n):
+def solution(arr, n):
     tmp = ""
-    for i in range(10):
-        
-        
+    for e in arr:
+        tmp += e[0]*e[1]
+    chunks, chunk_size = len(tmp), 10
+    return [tmp[i:i+chunk_size] for i in range(0, chunks, chunk_size)]
+
+
+
 t = int(input())
 for i in range(1, t+1):
-    answer = ""
     n = int(input())
+    arr = []
     for _ in range(n):
-        ch, num = input().split()
-        answer += solution(answer, ch, num)
-    print("#{} {}".format(i, answer))
+        c, m = input().split()
+        arr.append([c, int(m)])
+        
+    print("#{}".format(i))
+    answer = solution(arr, n)
+    for e in answer:
+        print(e)
--- samsung sw/level2/mode.py
+++ samsung sw/level2/mode.py
@@ -1,13 +1,15 @@
-# 이걸 왜 못푸냐 빙신아
+import collections
 
 def solution(scores):
-    score_count = Counter(scores)
-    answer = sorted(score_count, key=score_count.get)[-2]
-    return answer        
+    data = collections.Counter(scores)
+    data_list = dict(data)
+    max_value = max(list(data.values()))
+    mode_val = [num for num, freq in data_list.items() if freq == max_value]
+    return max(mode_val)
 
 t = int(input())
-for testcase in range(1, t+1):
-    answer = 0
+for i in range(1, t+1):
+    n = int(input())
     scores = list(map(int, input().split()))
     answer = solution(scores)
-    print("#{} {}".format(testcase, answer))
+    print("#{} {}".format(i, answer))
