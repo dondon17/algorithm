2afd3deedd20195d024db23e6e2f72b4c4fefdb7
dondon <qwerqw889@ajou.ac.kr>
2021-03-31 00:11:40 +0900

    update

--- samsung sw/level2/snailnum.py
+++ samsung sw/level2/snailnum.py
@@ -1,17 +1,53 @@
-def solution(N):
-    snailarr = [[0]*n]*n
-    val = 1
+def printarr(arr):
+    for r in arr:
+        for e in r:
+            print(e, end=" ")
+        print()
 
-    direction = ["R", "D", "L", "U"]
-    curx, cury = 0, 0
+def solution(size):
+    snailarr = [[0 for _ in range(size)] for _ in range(size)]
+    val = 0
 
+    x, y = 0, -1
+    sw = 1
+
+    while True:
+        for _ in range(size):
+            val += 1
+            y += sw
+            snailarr[x][y] = val
+            
+        size -= 1
+        if size == 0: break
+
+        for _ in range(size):
+            val += 1
+            x += sw
+            snailarr[x][y] = val
+            
+        sw *= -1
+         
+        for _ in range(size):
+            val += 1
+            y += sw
+            snailarr[x][y] = val
+            
+        size -= 1
+        if size == 0: break
+        for _ in range(size):
+            val += 1
+            x += sw
+            snailarr[x][y] = val
+            
+        sw *= -1
+
+    printarr(snailarr)
 
 
 
         
-
 t = int(input())
 for i in range(1, t+1):
-    n = int(input())
+    size = int(input())
     print("#{}".format(i))
-    solution(n)
+    solution(size)
