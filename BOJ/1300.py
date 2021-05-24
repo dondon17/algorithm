db0e6b1d019ba9db4b52246a50ddaea25c5e5951
dondon <qwerqw889@ajou.ac.kr>
2021-05-24 23:55:45 +0900

    update

--- BOJ/1300.py
+++ BOJ/1300.py
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
-n = int(input())
-k = int(input())
-arr = []
-
-for i in range(n):
-    for j in range(n):
-        arr.append((i+1)*(j+1))
-
-arr.sort()
-print(arr[k])
-=======
-from types import ClassMethodDescriptorType
 import sys
 put = sys.stdin.readline
 
 n = int(put().strip())
 k = int(put().strip())
-
-fo
->>>>>>> dondon
