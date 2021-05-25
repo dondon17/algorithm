d314477637753d77fb49786cf00b4aa481639e29
dondon <qwerqw889@ajou.ac.kr>
2021-05-25 18:58:40 +0900

    BOJ k번째 수 (이분탐색)

--- BOJ/1300.py
+++ BOJ/1300.py
@@ -1,5 +1,23 @@
 import sys
 put = sys.stdin.readline
 
-n = int(put().strip())
-k = int(put().strip())
+n = int(put())
+k = int(put())
+
+low = 0
+high = k
+
+answer = 0
+while low <= high:
+    mid = (low+high)//2
+    cnt = 0
+    for i in range(1, n+1):
+        cnt += min(mid//i, n)
+
+    if cnt < k:
+        low = mid+1
+    else:
+        answer = mid
+        high = mid-1
+
+print(answer)
